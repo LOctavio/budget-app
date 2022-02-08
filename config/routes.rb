64ae0dc509d3128c52b166860a4e9c5bc2670b78
LOctavio <luis_ram1997@hotmail.com>
2022-02-08 14:44:51 -0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,10 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :products
-  resources :groups
+  
+  resources :groups do
+    resources :products
+  end
   devise_scope :user do
     get 'signout', to: 'users/sessions#destroy'
   end
