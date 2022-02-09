b332422c8068b1b88963368a6362a6c154dc4afa
LOctavio <luis_ram1997@hotmail.com>
2022-02-08 19:49:15 -0600

    Add 'categories' and 'add a new category' pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,25 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  def index
+    @groups = current_user.groups.all
+  end
+
+  def new
+    @group = current_user.groups.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    if @group.save!
+      redirect_to root_path
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private 
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
