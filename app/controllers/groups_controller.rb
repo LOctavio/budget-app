140d3b8f47fb4e5696056c71875785a5882c3a11
LOctavio <luis_ram1997@hotmail.com>
2022-02-08 19:56:37 -0600

    Add flash messages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,10 +10,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = current_user.groups.new(group_params)
-    if @group.save!
+    if @group.save
       redirect_to root_path
+      flash[:alert] = 'Your category was created successfully'
     else
       redirect_back(fallback_location: root_path)
+      flash[:alert] = 'Your category was not created'
     end
   end
 
